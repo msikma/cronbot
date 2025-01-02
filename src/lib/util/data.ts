101ee5f64a3fcc62bbc27ebf1eb2d6cb48339230
Michiel Sikma <michiel@sikma.org>
2025-01-02 22:33:30 +0100

    Add promiseSequential() for rate limiting purposes

--- src/lib/util/data.ts
+++ src/lib/util/data.ts
@@ -9,3 +9,19 @@ import {omitBy, isUndefined} from 'lodash-es'
 export function omitUndefined(obj: object): object {
   return omitBy(obj, isUndefined)
 }
+
+/**
+ * Runs promises sequentially.
+ * 
+ * Call like Promise.all().
+ */
+export function promiseSequential<T>(promises: Promise<T>[]): Promise<T[]> {
+  return promises.reduce(
+    async (chain, current) => {
+      const results = await chain
+      const result = await current
+      return [...results, result]
+    },
+    Promise.resolve([] as T[])
+  )
+}
