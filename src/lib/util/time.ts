e53cb622e16de931119ae6118c305f6326ea7240
Michiel Sikma <michiel@sikma.org>
2025-01-07 20:26:04 +0100

    Add formatDuration function

--- src/lib/util/time.ts
+++ src/lib/util/time.ts
@@ -1,6 +1,8 @@
 // cpl-apollo <https://github.com/msikma/cpl-apollo>
 // © MIT license
 
+import {default as _formatDuration} from 'format-duration'
+
 // Time units accepted by scheduleEvery().
 export type TimeUnit = 'second' | 'seconds' | 'minute' | 'minutes' | 'hour' | 'hours'
 
@@ -17,11 +19,18 @@ export const sleep: (ms: number) => Promise<void> = (ms) => new Promise(resolve 
 /**
  * Returns a milliseconds value for a given number and time unit.
  */
-export function scheduleEvery(n: number, unit: TimeUnit) {
+export function scheduleEvery(n: number, unit: TimeUnit): number {
   const unitSingular = unit.replace(/s$/, '')
   const ms = unitMilliseconds.get(unitSingular)
   if (ms === undefined) {
     throw new Error(`Invalid time unit: ${unit}`)
   }
   return n * ms
+}
+
+/**
+ * Formats a given number of milliseconds as a string.
+ */
+export function formatDuration(ms: number): string {
+  return _formatDuration(ms, {ms: false})
 }
